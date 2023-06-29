64a2dd7acc4419fecd113c241e484d1783fcacad
arshia.mkh <arshia.mokhlesi@gmail.com>
2023-06-29 12:50:24 +0330

    add status enum

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 
+from pip._internal.utils.misc import enum
+
 
 # Note : define the Resources data class (struct)
 @dataclass
@@ -12,7 +14,7 @@ R2 = Resource(0)
 R3 = Resource(0)
 
 
-# Note : define the type of tasks and then initial them
+# Note : define the type of tasks dataclass and then initial them
 @dataclass
 class Task_type:
     priority: int
@@ -22,3 +24,10 @@ class Task_type:
 x = Task_type(3, [R1, R2])
 y = Task_type(2, [R2, R3])
 z = Task_type(1, [R1, R3])
+
+
+# here we will define an enum to determine the status of a process
+class Status(enum):
+    running = 0
+    ready = 1
+    waiting = 2
