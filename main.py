cba7c57b265cde609cd3d858f908d34b1a0828fa
arshia.mkh <arshia.mokhlesi@gmail.com>
2023-06-29 12:44:57 +0330

    add task type dataclass and initial it

--- main.py
+++ main.py
@@ -6,6 +6,19 @@ from dataclasses import dataclass
 class Resource:
     number: int
 
+
 R1 = Resource(0)
 R2 = Resource(0)
 R3 = Resource(0)
+
+
+# Note : define the type of tasks and then initial them
+@dataclass
+class Task_type:
+    priority: int
+    resource_type: list[Resource]
+
+
+x = Task_type(3, [R1, R2])
+y = Task_type(2, [R2, R3])
+z = Task_type(1, [R1, R3])
