717f403bea18a22da3712d4ee99f972fdce8192c
arshia.mkh <arshia.mokhlesi@gmail.com>
2023-06-29 12:44:12 +0330

    add resource dataclass

---
+++ main.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+
+# Note : define the Resources data class (struct)
+@dataclass
+class Resource:
+    number: int
+
+R1 = Resource(0)
+R2 = Resource(0)
+R3 = Resource(0)
